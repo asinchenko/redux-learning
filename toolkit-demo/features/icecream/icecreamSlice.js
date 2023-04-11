49b52312e7775a7056e73a63d23479592dcea3f5
asinchenko <wonderfulsanchez@gmail.com>
2023-04-12 00:09:43 +0600

    redux-tools

---
+++ toolkit-demo/features/icecream/icecreamSlice.js
@@ -0,0 +1,26 @@
+const createSlice = require('@reduxjs/toolkit').createSlice
+const cakeActions = require('../cake/cakeSlice').cakeActions
+
+const initialState = {
+    numOfIcecream: 20,
+}
+const icecreamSlice = createSlice({
+    name: 'icecream',
+    initialState,
+    reducers: {
+        ordered: (state, action) => {
+            state.numOfIcecream-- 
+        },
+        restocked : (state, action) => {
+            state.numOfIcecream += action.payload
+        }
+    },
+    extraReducers: (builder) => {
+        // builder.addCase(cakeActions.ordered, (state) => {
+        //     state.numOfIcecream--
+        // })
+    }
+})
+
+module.exports = icecreamSlice.reducer
+module.exports.icecreamActions = icecreamSlice.actions
---
+++ toolkit-demo/index.js
@@ -0,0 +1,15 @@
+const store = require('./app/store')
+const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
+const fetchUsers = require('./features/user/userSlice').fetchUsers
+
+console.log("initial state: ",store.getState())
+
+const unsubscribe = store.subscribe(() => {console.log(store.getState())})
+
+
+store.dispatch(cakeActions.ordered()) 
+store.dispatch(cakeActions.ordered()) 
+store.dispatch(cakeActions.ordered()) 
+store.dispatch(cakeActions.restocked(3)) 
+store.dispatch(fetchUsers())
